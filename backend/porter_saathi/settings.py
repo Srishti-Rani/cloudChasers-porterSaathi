91a20aa7082164855f0f0beb3bf9ec136c9e148c
Sarita Chaudhary <sarita.chaudhary@coverstack.in>
2025-09-13 16:27:01 +0530

    Fixed the file path error

--- backend/porter_saathi/settings.py
+++ backend/porter_saathi/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,3 +138,6 @@ CORS_ALLOWED_ORIGINS = [
     " http://localhost:5173",   # your React dev server
     "http://127.0.0.1:3000",
 ]
+
+
+
